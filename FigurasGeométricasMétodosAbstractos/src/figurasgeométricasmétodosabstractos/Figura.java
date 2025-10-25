baac79aaacee319dc3b628afcdaa77ee50239661
donramones <agusrivarolaa00@gmail.com>
2025-10-25 08:23:41 -0300

    10ok

---
+++ FigurasGeométricasMétodosAbstractos/src/figurasgeométricasmétodosabstractos/Figura.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package figurasgeométricasmétodosabstractos;
+
+/**
+ *
+ * @author rivar
+ */
+public abstract class Figura {
+    private String nombre;
+
+    public Figura (String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public abstract double calcularArea();
+    
+    
+}
