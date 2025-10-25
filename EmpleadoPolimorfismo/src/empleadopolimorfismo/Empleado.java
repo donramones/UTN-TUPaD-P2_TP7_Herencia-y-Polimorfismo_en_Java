216d1f38018e8a4679ca1981bdddac1fe5dc732c
donramones <agusrivarolaa00@gmail.com>
2025-10-25 08:48:15 -0300

    10ok

--- EmpleadoPolimorfismo/src/empleadopolimorfismo/Empleado.java
+++ EmpleadoPolimorfismo/src/empleadopolimorfismo/Empleado.java
@@ -11,6 +11,13 @@ package empleadopolimorfismo;
 public abstract class Empleado {
     private String nombre;
     private String id;
+
+    public Empleado(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+    
+    
     
     public abstract double calcularSueldo();
     
